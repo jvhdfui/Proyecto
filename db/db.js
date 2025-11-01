298cf0442c9705583510896a18c2270e5a219d46
Ana Karen López <137356105+jvhdfui@users.noreply.github.com>
2025-10-31 20:25:52 -0600

    Update MongoDB connection to use environment variable

--- db/db.js
+++ db/db.js
@@ -4,11 +4,12 @@ async function conectarBD(){
 		/*//atlas
 		const conexion = await mongoose.connect("mongodb+srv://anakarenlori_db_user:Toto1183@cluster0.e2hlaur.mongodb.net/?retryWrites=true&w=majority&appName=Recetas")
 		compas*/
-		const conexion = await mongoose.connect("mongodb://localhost:27017/Recetas")
+		const conexion = await mongoose.connect(process.env.KEY_MONGO)
 		console.log("Conexión Exitosa con Mongo Compass")
 	}catch(err){
 		console.log("Error" + err)
 	}
 }
+
 
 export default conectarBD;
