cea2487d0cd2df04bda6546242f7f9ee7661885a
Ana Karen López <137356105+jvhdfui@users.noreply.github.com>
2025-10-31 21:45:23 -0600

    Refactor database connection logic in db.js

--- db/db.js
+++ db/db.js
@@ -4,13 +4,13 @@ import 'dotenv/config'
 async function conectarBD(){
 	//conexión a base
 	try{
-		//atlas
-		const conexion = await mongoose.connect("mongodb+srv://anakarenlori_db_user:<Toto1183>@cluster0.t6vvfu0.mongodb.net/?retryWrites=true&w=majority&appName=Recetas")
+		//atlas const conexion = await mongoose.connect("mongodb+srv://anakarenlori_db_user:<Toto1183>@cluster0.t6vvfu0.mongodb.net/?retryWrites=true&w=majority&appName=Recetas")
 		//compas
-		/*const conexion = await mongoose.connect(process.env.KEY_MONGO)*/
+		const conexion = await mongoose.connect(process.env.KEY_MONGO)
 		console.log("Conexión Exitosa con Mongo Compass")
 	}catch(err){
 		console.log("Error" + err)
 	}
 }
 export default conectarBD
+
