2e81671b78e38cbc0dfdbc220360f606031c0436
Ana Karen López <137356105+jvhdfui@users.noreply.github.com>
2025-11-24 16:18:18 -0600

    Add 'edad' and 'foto' fields to user schema

--- models/modelUser.js
+++ models/modelUser.js
@@ -1,10 +1,31 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    username:  { type: String, required: true },
-    email:     { type: String, required: true, unique: true },
-    telefono:  { type: String, required: true },
-    password:  { type: String, required: true }
+    username:  { 
+        type: String, 
+        required: true 
+    },
+    edad: {
+        type: Number, 
+        required: true
+    },
+    email: {
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+    telefono: { 
+        type: String, 
+        required: true 
+    },
+    password: { 
+        type: String, 
+        required: true 
+    },
+    foto: { 
+        type: String, 
+        default: null 
+    }
 });
 const User = mongoose.model("User", userSchema);
 export default User;
