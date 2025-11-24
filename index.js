0f1529ec49ea6d0e35c59ec0327bb0f83914d7d2
Ana Karen López <137356105+jvhdfui@users.noreply.github.com>
2025-11-24 16:20:47 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,39 @@
-import express from "express"
-import session from 'express-session';
-import rutas from "./routes/rutas.js"
-import conectarBD from "./db/db.js" 
+import express from "express";
+import session from "express-session";
+import rutas from "./routes/rutas.js";
+import conectarBD from "./db/db.js";
 
-const app = express()
-app.use(express.static('public')); //imagen
+const app = express();
 
-async function conexion(){
-  await conectarBD()
+//imagen
+app.use(express.static('public'));
+
+async function conexion() {
+  await conectarBD();
 }
+conexion();
 
-conexion()
-
-//cerrar sesión
+// Configuración de sesiones
 app.use(session({
-    secret: 'cookland_compartiendo_cocina', 
-    resave: false, 
-    saveUninitialized: false,
-    cookie: { 
-        secure: process.env.NODE_ENV === 'production',
-    }
+  secret: 'cookland_compartiendo_cocina',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: process.env.NODE_ENV === 'production'
+  }
 }));
+// Middleware para que usuario esté disponible en todas las vistas EJS
+app.use((req, res, next) => {
+    res.locals.usuario = req.session.usuario || null;
+    next();
+});
 
-app.use(express.urlencoded({extended:true}))
-app.set("view engine", "ejs")
-app.use("/", rutas)
+
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use("/", rutas);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, function () {
+app.listen(PORT, () => {
   console.log("Aplicación en http://localhost:" + PORT);
-})
+});
