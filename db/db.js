2d8bb5d766e2f3454845468d612aa6f595632f75
Ana Karen López <137356105+jvhdfui@users.noreply.github.com>
2025-11-27 21:08:48 -0600

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,13 +1,14 @@
 import mongoose from "mongoose"
 async function conectarBD(){
 	try{
-		/*//atlas
+		//atlas
 		const conexion = await mongoose.connect("mongodb+srv://anakarenlori_db_user:Toto1183@cluster0.e2hlaur.mongodb.net/?retryWrites=true&w=majority&appName=Recetas")
-		compas*/
-		const conexion = await mongoose.connect("mongodb://localhost:27017/Recetas")
+		compas
+		/*const conexion = await mongoose.connect("mongodb://localhost:27017/Recetas")*/
 		console.log("Conexión Exitosa con Mongo Compass")
 	}catch(err){
 		console.log("Error" + err)
 	}
 }
 export default conectarBD;
+
